7789c5fb46d8c779292b0c4a55386fca997e920e
* <*>
2024-03-21 18:33:24 +0800

    更新

--- save2github/app.js
+++ save2github/app.js
@@ -1,17 +1,12 @@
 import express from 'express'
 import bodyParser from 'body-parser'
+import cookieParser from 'cookie-parser'
 import httpProxy from 'http-proxy'
 import { oauthAuthorizationUrl} from '@octokit/oauth-authorization-url'
 import { createOAuthUserAuth } from '@octokit/auth-oauth-user'
 
 const dotenv = await import('dotenv')
 dotenv.default.config()
-
-console.log(process.env.GITHUB_CLIENT_ID)
-
-import tough from 'tough-cookie'
-var Cookie = tough.Cookie;
-
 
 const apiProxy = httpProxy.createProxyServer()
 const routers = express.Router()
@@ -21,55 +16,51 @@ const app = express()
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
+app.use(cookieParser())
 
 routers.get('/oauth/github', async (request, response) => {
-  console.log(request.re)
+  console.log(request.headers.referer)
+  let redirectBaseUrl = request.headers.referer || ''
   const { url, state } = oauthAuthorizationUrl({
     clientType: 'github-app',
     clientId: process.env.GITHUB_CLIENT_ID,
-    redirectUrl: 'http://localhost:3000/oauth/github.callback'
+    redirectUrl: `${redirectBaseUrl}oauth/github.callback`
     // login: "octocat",
     // state: "secret123",
   })
-  console.log(url, state)
-  response.cookie('state', state).redirect(302, url)
+  response.cookie('state', `${redirectBaseUrl}|${state}`).redirect(302, url)
 })
-
 
 routers.get('/oauth/github.callback', async (request, response) => {
-  const { code, state } = request.query
-  console.log(request) // 包含域名
-  var cookie = Cookie.parse(request.header)
-  console.log(cookie) //
-  // console.log(request.cookies.state) // 包含域名
-  console.log(code)
+  try {
+    const { code, state } = request.query
 
-  const auth = createOAuthUserAuth({
-    clientId: process.env.GITHUB_CLIENT_ID,
-    clientSecret: process.env.GITHUB_CLIENT_SECRET,
-    clientType: 'github-app',
-    code,
-    state
-  })
-  // resolves once the user entered the `user_code` on `verification_uri`
-  const { token } = await auth()
-  console.log(token)
-  // try {
-  //   const api = (await import('./github/index.js')).default
-  //   response.json(await api(code))
-  // } catch (error_) {
-  //   console.log(error_)
-  //   response.status(404).send()
-  // }
+    console.log(request.cookies) // 包含域名
+    let cookies = request.cookies
+    let [redirectBaseUrl, cookieState ] = (cookies.state || '').split('|')
+    const auth = createOAuthUserAuth({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      clientType: 'github-app',
+      code,
+      state
+      // expiresAt: '',
+      // refreshTokenExpiresAt: ''
+    })
+    const { token, refreshToken,  } = await auth()
 
-  response.cookie('accessToken', token).redirect(302, 'https://localhost:8087/admin')
+    response.cookie('accessToken', token).redirect(302, redirectBaseUrl)
+  } catch(err) {
+    console.log(err)
+    response.status(500).send(err)
+  }
 })
 
-// routers.all('/*', function(req, res) {
-//   apiProxy.web(req, res, {
-//     target: 'http://localhost:8080/'
-//   })
-// })
+routers.all('/*', function(req, res) {
+  apiProxy.web(req, res, {
+    target: 'http://localhost:8080/'
+  })
+})
 
 app.use('/', routers)
 
